47d2055896584171cdf5ba02c3de10a053c6bef1
cqder03 <126189235+cqder03@users.noreply.github.com>
2024-02-13 21:59:21 +0100

    Initial and final exercise commit

---
+++ fibonacci.js
@@ -0,0 +1,28 @@
+function fibs(n) {
+    const arr = [];
+    let x = 0;
+    let y = 1;
+    let z;
+    arr.push(x, y);
+    for (let i = 2; i <= n; i++) {
+        z = x + y;
+        x = y;
+        y = z;
+        arr.push(z);
+    }
+}
+
+function fibsRec(n) {
+    if (n === 0) return [0];
+    if (n === 1) return [1];
+
+    let fibNumbers = fibsRec(n - 1);
+
+    if (n === 2) {
+        fibNumbers.push(1);
+    } else {
+        fibNumbers.push(fibNumbers[fibNumbers.length - 1] + fibNumbers[fibNumbers.length - 2]);
+    }
+
+    return fibNumbers;
+}
---
+++ mergeSort.js
@@ -0,0 +1,32 @@
+function mergeSort(array) {
+    if (array.length < 2) {
+        return array;
+    }
+
+    const mid = Math.floor(array.length / 2);
+    let left = mergeSort(array.slice(0, mid));
+    let right = mergeSort(array.slice(mid));
+    let merge = [];
+    let i = 0;
+    let j = 0;
+
+    while (i < left.length && j < right.length) {
+        if (left[i] < right[j]) {
+            merge.push(left[i]);
+            i++;
+        } else {
+            merge.push(right[j]);
+            j++;
+        }
+    }
+
+    for (i; i < left.length; i++) {
+        merge.push(left[i]);
+    }
+    
+    for (j; j < right.length; j++) {
+        merge.push(right[j]);
+    }
+
+    return merge
+}
